155e909a3384429c7928c56f7096901c41c679a1
Emily Ma <emilyma@outlook.com>
2025-06-15 13:21:57 -0700

    feat: nav bar

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-// Fade-in animation using Intersection Observer
+//Smooth scroll effect
+const lenis = new Lenis();
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+/* Nav Bar Link Scrolling */
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      const targetID = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetID);
+  
+      if (targetElement) {
+        const navHeight = document.querySelector('nav').offsetHeight;
+        const elementPosition = targetElement.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.pageYOffset - navHeight - 10; // 10px extra offset
+  
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+
+// Auto-scroll gallery
 const cards = document.querySelectorAll('.product-card');
 
 const observer = new IntersectionObserver((entries) => {
